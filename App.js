1160d8197506a80a90bdffe45eda977f1ffc99dd
Moshiur Rahman <mralif004@gmail.com>
2021-05-17 11:50:38 -0300

    added all functionality of home page

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 export default function App() {
 	const [tasks, setTasks] = useState([
@@ -14,24 +15,40 @@ export default function App() {
 		{
 			id: 1,
 			title: "Call Rahman",
-			time: "May 19st 10pm",
+			time: "May 19th 10pm",
 			reminder: false,
 		},
 	]);
 
-	// const [showForm, setShowForm] = useState(false);
+  const [formOpen, setFormOpen] = useState(false);
 
-	const [idCounter, setIdCounter] = useState(1);
+	const [idCounter, setIdCounter] = useState(2);
 
 	// open Form event
 	const openForm = () => {
-		console.log("button pressed");
+		// console.log("button pressed");
+    setFormOpen(!formOpen);
 	};
+
+//delete event
+const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+}
+
+//addTask
+const addTask = task => {
+		const newTask = { ...task, id: idCounter };
+
+		setIdCounter(idCounter + 1);
+		setTasks([...tasks, newTask]);
+	};
+
 
 	return (
 		<View style={styles.container}>
-			<Header header={"Task Tracker"} showForm={openForm} />
-			<Tasks tasks={tasks} />
+			<Header header={"Task Tracker"} showForm={openForm} openform={formOpen} style={styles.header}/>
+      {formOpen && <AddTask onSave={addTask}/>}
+			{tasks.length < 1 ? <Text> No task to track</Text> : <Tasks tasks={tasks} onDelete={deleteTask} />}
 		</View>
 	);
 }
@@ -40,6 +57,13 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#fff",
-		alignItems: "center",
+		// alignItems: "center",
+    // borderColor: "blue",
+		// borderWidth: 2,
+    paddingLeft: 20,
+    paddingRight: 20,
 	},
+  header: {
+    marginBottom: 10,
+  }
 });
