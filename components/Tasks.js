c60ee6d138538880b29cd5730149edc727087f30
Moshiur Rahman <mralif004@gmail.com>
2021-05-17 11:54:22 -0300

    added event to delete task

--- components/Tasks.js
+++ components/Tasks.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Task from "./Task"
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
 	return (
 		<View>
 			{tasks.map(task => (
-				<Text key={task.id}>{task.title}</Text>
+				<Task key={task.id} id={task.id} title={task.title} time={task.time} onDelete={onDelete} />
 			))}
 		</View>
 	);
