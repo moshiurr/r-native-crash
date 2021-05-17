d07983e5fe2ec37963b7b80077f88d9ce10eb255
Moshiur Rahman <mralif004@gmail.com>
2021-05-17 11:53:23 -0300

    header.js updated

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
 
-const Header = ({ header, showForm }) => {
+const Header = ({ header, showForm, openform }) => {
 	return (
 		<View style={style.header}>
 			<Text style={style.headerSize}>
-				{header} <Button title="Add" onPress={showForm} />{" "}
+				{header}
 			</Text>
+      <Button title={openform ? "Close" : "Add"} color={openform ? "black" : "skyblue"} onPress={showForm} />
 		</View>
 	);
 };
@@ -14,12 +15,15 @@ const Header = ({ header, showForm }) => {
 const style = StyleSheet.create({
 	header: {
 		// flex: 1,
-		// flexDirection: "row",
-		// justifyContent: "space-around",
+		flexDirection: "row",
+		justifyContent: "space-around",
+    // alignSelf: "streatch",
 		// alignItems: "flex-start",
-		marginTop: 30,
-		borderColor: "red",
-		borderWidth: 2,
+		marginTop: 40,
+		// borderColor: "red",
+		// borderWidth: 2,
+    
+    marginBottom: 10,
 	},
 	headerSize: {
 		fontSize: 25,
